5a3c5246ebb4c00fa5b1b7cc078c7bffa71e3411
Shinnosuke Yasuda <48393662+CrayonSY@users.noreply.github.com>
2021-12-06 00:24:17 -0800

    contactのリンクとmenu.jsを消した

--- menu.js
+++ menu.js
@@ -1,19 +1,19 @@
-(jQuery)(function($) {
-    var opened = false;
-    $('.btn').on('click',function(){
-        $(this).toggleClass('open');
-        if(opened){
-            $('.nav-list').stop(true).animate({
-                left: '-40rem'
-            }, 300);
-            opened = false;
-        }
-        else{
-            $('.nav-list').stop(true).animate({
-                left: 0
-            }, 300);
-            opened=true;
-        }
-        return false;
-    });
-});
+// (jQuery)(function($) {
+//     var opened = false;
+//     $('.btn').on('click',function(){
+//         $(this).toggleClass('open');
+//         if(opened){
+//             $('.nav-list').stop(true).animate({
+//                 left: '-40rem'
+//             }, 300);
+//             opened = false;
+//         }
+//         else{
+//             $('.nav-list').stop(true).animate({
+//                 left: 0
+//             }, 300);
+//             opened=true;
+//         }
+//         return false;
+//     });
+// });
